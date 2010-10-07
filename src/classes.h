fe6175522ceaa11aa12368bc24c2a51af523bcfe
cmsbuild <>
2010-10-07 01:42:11 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- SelectorUtils-V00-02-22'.
    Sprout from PhysicsTools-SelectorUtils-B3_8_X 2010-10-07 01:42:10 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Updating to top v4 selection'
    Cherrypick from master 2010-08-09 13:52:27 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Adding classes files':
        src/classes.h
        src/classes_def.xml

--- src/classes.h
+++ src/classes.h
@@ -2,7 +2,6 @@
 
 //Add includes for your classes here
 #include "PhysicsTools/SelectorUtils/interface/strbitset.h"
-#include "DataFormats/Common/interface/Wrapper.h"
 #include "PhysicsTools/SelectorUtils/interface/JetIDSelectionFunctor.h"
 #include "PhysicsTools/SelectorUtils/interface/EventSelector.h"
 #include "PhysicsTools/SelectorUtils/interface/ElectronVPlusJetsIDSelectionFunctor.h"
@@ -10,17 +9,3 @@
 #include "PhysicsTools/SelectorUtils/interface/PFJetIDSelectionFunctor.h"
 #include "PhysicsTools/SelectorUtils/interface/PVSelector.h"
 #include "PhysicsTools/SelectorUtils/interface/RunLumiSelector.h"
-
-
-namespace {
-  struct dictionary {
-
-    std::map<std::basic_string<char>,unsigned int> internalMap;
-    pat::strbitset strbitset;
-    edm::Wrapper<pat::strbitset> wstrbitset;
-    std::vector< pat::strbitset> vstrbitset;
-    edm::Wrapper< std::vector< pat::strbitset> > wvstrbitset;
-    
-  };
-
-}
